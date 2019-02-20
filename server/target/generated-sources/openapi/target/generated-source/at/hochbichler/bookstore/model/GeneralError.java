a6250df3192892e54abc9d5e0fc9c0cae8c122f4
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2019-02-20 16:18:45 +0100

    initial version

---
+++ server/target/generated-sources/openapi/target/generated-source/at/hochbichler/bookstore/model/GeneralError.java
@@ -0,0 +1,109 @@
+package at.hochbichler.bookstore.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * GeneralError
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-02-20T16:17:52.413+01:00[Europe/Berlin]")
+
+public class GeneralError  implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @JsonProperty("code")
+  private String code;
+
+  @JsonProperty("description")
+  private String description;
+
+  public GeneralError code(String code) {
+    this.code = code;
+    return this;
+  }
+
+  /**
+   * Get code
+   * @return code
+  */
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public GeneralError description(String description) {
+    this.description = description;
+    return this;
+  }
+
+  /**
+   * Get description
+   * @return description
+  */
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    GeneralError generalError = (GeneralError) o;
+    return Objects.equals(this.code, generalError.code) &&
+        Objects.equals(this.description, generalError.description);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(code, description);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class GeneralError {\n");
+    
+    sb.append("    code: ").append(toIndentedString(code)).append("\n");
+    sb.append("    description: ").append(toIndentedString(description)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
